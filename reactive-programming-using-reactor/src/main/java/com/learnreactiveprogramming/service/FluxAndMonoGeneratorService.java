51083a9b501e01bf877505b04f027ba92b064777
ahmed shalash <ashalash38@gmail.com>
2023-02-24 17:35:23 +0200

    update flux and monos

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,38 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+/**
+ * @Author Ahmed Shalash
+ */
 public class FluxAndMonoGeneratorService {
+
+
+    public Flux<String> stringFlux(){
+        return Flux.fromIterable(List.of("ahmed" , "ayla" , "shalash")).log(); // maybe come from api or db
+    }
+
+    public Mono<String> stringMono(){
+        return Mono.just("Shalash");
+    }
+
+    public static void main(String[] args) {
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+        fluxAndMonoGeneratorService.stringFlux()
+                .subscribe(name->{
+                    System.out.println( " Flux is " + name);
+                });
+
+        fluxAndMonoGeneratorService.stringMono().subscribe( name -> {
+            System.out.println( "Mono is ->" + name);
+        });
+
+    }
+
+
 }
